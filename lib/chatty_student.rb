f311d49455973fd586c2c875e3115eb7cfff95ed
Ryan Hollins <ballbaroi@gmail.com>
2020-03-20 17:10:43 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,7 @@
 require "pry"
 class ChattyStudent
   def hello
-    puts {chatty_student.hello}
+    puts "Hey there! I'm so excited to learn stuff.
+    How are you doing today? I' okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man"
   end
 end
